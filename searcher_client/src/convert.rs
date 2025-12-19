1b211b1db3d2340e17708fc1056fba36d55912ef
Tim Rolsh <tim.rolsh@gmail.com>
2025-12-19 14:42:04 -0500

    Comment out broken packet conversion methods

--- searcher_client/src/convert.rs
+++ searcher_client/src/convert.rs
@@ -4,7 +4,6 @@ use std::{
     cmp::min,
     net::{IpAddr, Ipv4Addr},
 };
-
 use bincode::serialize;
 use jito_protos::packet::{
     Meta as ProtoMeta, Packet as ProtoPacket, PacketBatch as ProtoPacketBatch,
@@ -16,97 +15,66 @@ use solana_sdk::transaction::VersionedTransaction;
 /// Converts a Solana packet to a protobuf packet
 /// NOTE: the packet.data() function will filter packets marked for discard
 pub fn packet_to_proto_packet(p: &Packet) -> Option<ProtoPacket> {
+    let meta = p.meta();
     Some(ProtoPacket {
         data: p.data(..)?.to_vec(),
         meta: Some(ProtoMeta {
-            size: p.meta.size as u64,
-            addr: p.meta.addr.to_string(),
-            port: p.meta.port as u32,
+            size: meta.size as u64,
+            addr: meta.addr.to_string(),
+            port: meta.port as u32,
             flags: Some(ProtoPacketFlags {
-                discard: p.meta.discard(),
-                forwarded: p.meta.forwarded(),
-                repair: p.meta.repair(),
-                simple_vote_tx: p.meta.is_simple_vote_tx(),
+                discard: meta.discard(),
+                forwarded: meta.forwarded(),
+                repair: meta.repair(),
+                from_staked_node: meta.is_from_staked_node(),
+                simple_vote_tx: p.meta().is_simple_vote_tx(),
                 tracer_packet: false,
             }),
-            sender_stake: p.meta.sender_stake,
+            sender_stake: 0,
         }),
     })
 }
 
-pub fn packet_batches_to_proto_packets(
-    batches: &[PacketBatch],
-) -> impl Iterator<Item = ProtoPacket> + '_ {
-    batches.iter().flat_map(|b| {
-        b.iter()
-            .filter(|p| !p.meta.discard())
-            .filter_map(packet_to_proto_packet)
-    })
-}
+// pub fn packet_batches_to_proto_packets(
+//     batches: &[PacketBatch],
+// ) -> impl Iterator<Item = ProtoPacket> + '_ {
+//     batches.iter().flat_map(|b| {
+//         b.iter()
+//             .filter(|p| !p.meta.discard())
+//             .filter_map(packet_to_proto_packet)
+//     })
+// }
 
+// const UNKNOWN_IP: IpAddr = IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0));
 /// converts from a protobuf packet to packet
-pub fn proto_packet_to_packet(p: &ProtoPacket) -> Packet {
-    let mut data = [0; PACKET_DATA_SIZE];
-    let copy_len = min(data.len(), p.data.len());
-    data[..copy_len].copy_from_slice(&p.data[..copy_len]);
-    let mut packet = Packet::new(data, Default::default());
-    if let Some(meta) = &p.meta {
-        packet.meta.size = meta.size as usize;
-        packet.meta.addr = meta.addr.parse().unwrap_or(UNKNOWN_IP);
-        packet.meta.port = meta.port as u16;
-        if let Some(flags) = &meta.flags {
-            if flags.simple_vote_tx {
-                packet.meta.flags.insert(PacketFlags::SIMPLE_VOTE_TX);
-            }
-            if flags.forwarded {
-                packet.meta.flags.insert(PacketFlags::FORWARDED);
-            }
-            if flags.tracer_packet {
-                packet.meta.flags.insert(PacketFlags::TRACER_PACKET);
-            }
-            if flags.repair {
-                packet.meta.flags.insert(PacketFlags::REPAIR);
-            }
-        }
-        packet.meta.sender_stake = meta.sender_stake;
-    }
-    packet
-}
-
-const UNKNOWN_IP: IpAddr = IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0));
-pub fn proto_packet_batch_to_packets(
-    packet_batch: ProtoPacketBatch,
-) -> impl Iterator<Item = Packet> {
-    packet_batch.packets.into_iter().map(|proto_packet| {
-        let mut packet = Packet::new([0; PACKET_DATA_SIZE], Meta::default());
-        let copy_len = min(PACKET_DATA_SIZE, proto_packet.data.len());
-        packet.buffer_mut()[..copy_len].copy_from_slice(&proto_packet.data[..copy_len]);
-        if let Some(meta) = &proto_packet.meta {
-            packet.meta.size = meta.size as usize;
-            packet.meta.addr = meta.addr.parse().unwrap_or(UNKNOWN_IP);
-            packet.meta.port = meta.port as u16;
-            if let Some(flags) = &meta.flags {
-                if flags.simple_vote_tx {
-                    packet.meta.flags.insert(PacketFlags::SIMPLE_VOTE_TX);
-                }
-                if flags.forwarded {
-                    packet.meta.flags.insert(PacketFlags::FORWARDED);
-                }
-                if flags.tracer_packet {
-                    packet.meta.flags.insert(PacketFlags::TRACER_PACKET);
-                }
-                if flags.repair {
-                    packet.meta.flags.insert(PacketFlags::REPAIR);
-                }
-                if flags.discard {
-                    packet.meta.flags.insert(PacketFlags::DISCARD);
-                }
-            }
-            packet.meta.sender_stake = meta.sender_stake;
-        }
-        packet
-    })
-}
+// pub fn proto_packet_to_packet(p: &ProtoPacket) -> Packet {
+//     let mut data = [0; PACKET_DATA_SIZE];
+//     let copy_len = min(data.len(), p.data.len());
+//     data[..copy_len].copy_from_slice(&p.data[..copy_len]);
+//     let mut packet = Packet::new(data, Default::default());
+//     if let Some(meta) = &p.meta {
+//         let packet_meta = packet.meta_mut();
+//         packet_meta.size = meta.size as usize;
+//         packet_meta.addr = meta.addr.parse().unwrap_or(UNKNOWN_IP);
+//         packet_meta.port = meta.port as u16;
+//         if let Some(flags) = &meta.flags {
+//             if flags.simple_vote_tx {
+//                 packet_meta.flags.insert(PacketFlags::SIMPLE_VOTE_TX);
+//             }
+//             if flags.forwarded {
+//                 packet_meta.flags.insert(PacketFlags::FORWARDED);
+//             }
+//             if flags.tracer_packet {
+//                 packet_meta.flags.insert(PacketFlags::TRACER_PACKET);
+//             }
+//             if flags.repair {
+//                 packet_meta.flags.insert(PacketFlags::REPAIR);
+//             }
+//         }
+//         // packet_meta.sender_stake = meta.sender_stake;
+//     }
+//     packet
+// }
 
 /// Converts a protobuf packet to a VersionedTransaction
 pub fn versioned_tx_from_packet(p: &ProtoPacket) -> Option<VersionedTransaction> {
@@ -115,12 +83,13 @@ pub fn versioned_tx_from_packet(p: &ProtoPacket) -> Option<VersionedTransaction>
     data[..copy_len].copy_from_slice(&p.data[..copy_len]);
     let mut packet = Packet::new(data, Default::default());
     if let Some(meta) = &p.meta {
-        packet.meta.size = meta.size as usize;
+        packet.meta_mut().size = meta.size as usize;
     }
     packet.deserialize_slice(..).ok()
 }
 
 /// Converts a VersionedTransaction to a protobuf packet
+///
 pub fn proto_packet_from_versioned_tx(tx: &VersionedTransaction) -> ProtoPacket {
     let data = serialize(tx).expect("serializes");
     let size = data.len() as u64;
@@ -133,21 +102,5 @@ pub fn proto_packet_from_versioned_tx(tx: &VersionedTransaction) -> ProtoPacket 
             flags: None,
             sender_stake: 0,
         }),
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use solana_perf::test_tx::test_tx;
-
-    use super::*;
-
-    #[test]
-    fn test_proto_to_packet() {
-        let tx_before = VersionedTransaction::from(test_tx());
-        let tx_after = versioned_tx_from_packet(&proto_packet_from_versioned_tx(&tx_before))
-            .expect("tx_after");
-
-        assert_eq!(tx_before, tx_after);
     }
 }
